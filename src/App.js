3313e3f6ded0faece6d170f75792785203a36324
navyjeongs <wjdtjr8649@naver.com>
2022-11-02 20:43:51 +0900

    [11-02] solve App.js crash

--- src/App.js
+++ src/App.js
@@ -5,18 +5,28 @@ import { Route, Routes } from "react-router-dom";
 import GlobalStyle from "./GlobalStyles";
 import Stats from "./pages/Stats/Stats";
 import Summary from "./pages/Summary/Summary";
-
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import MyAccountPage from "./pages/MyAccountPage";
+import UserList from "./components/UserList";
+import Pagination from "./components/Pagination";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Register" element={<RegisterPage />} />
+        <Route path="/MyAccount" element={<MyAccountPage />} />
         <Route path="/stats" element={<Stats />}></Route>
         <Route path="/summary" element={<Summary />}></Route>
-        <Route path="/header" element={<Header />}></Route>
         <Route path="/rank" element={<TodayRank />}></Route>
         <Route path="/keyword" element={<KeywordContent />}></Route>
+        <Route path="/test" element={<UserList />}></Route>
       </Routes>
     </>
   );
