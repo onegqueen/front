f03e633637190d49fc89ed1c02861f796e126ee3
박정석 <wjdtjr8649@Naver.com>
2022-11-19 19:19:46 +0900

    [11-19] add setupProxy.js file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,12 +2,15 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware("/api", {
-      target: "http://localhost:8080",
+    "/apis",
+    createProxyMiddleware({
+      target: "https://192.168.0.5:8080",
       changeOrigin: true,
       pathRewrite: {
-        "^/api": "",
+        "^/apis": "",
       },
+      secure: false,
+      // [hpm] error occurred while proxying request 해결법
     })
   );
 };
