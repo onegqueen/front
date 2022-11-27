1ea63f3862624114d8f12dac92081649a51a742a
Sun Yerim <syl1602@naver.com>
2022-11-27 19:33:32 +0900

    [11-27] login, register etc 변경

--- src/components/MyAccount/MyAccount.jsx
+++ src/components/MyAccount/MyAccount.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import S from "./styled";
 import Timer from "../MyAccount/Timer/Timer";
+import "./Nickchange";
 
-function MyAccount() {
-  return <Timer />;
-}
+const MyAccount = () => {
+  const navigate = useNavigate();
+  const [nickname, setNickname] = useState("");
+
+  // 닉네임 변경 페이지
+  const navigateTochangeNick = () => {
+    navigate("/Nickchange");
+  };
+
+  return (
+    <div>
+      <S.logout type="button">로그아웃</S.logout>
+      <S.nickCheck type="button" onClick={navigateTochangeNick}>
+        닉네임 변경
+      </S.nickCheck>
+      <Timer />
+    </div>
+  );
+};
 
 export default MyAccount;
